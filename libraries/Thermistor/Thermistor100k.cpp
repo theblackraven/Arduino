6430200ef64039052bcb53af86433dfbe6cd20ac
theblackraven <theblackraven@web.de>
2015-09-13 18:57:22 +0200

    Lieber die kleinere Temperatur nehmen

--- libraries/Thermistor/Thermistor100k.cpp
+++ libraries/Thermistor/Thermistor100k.cpp
@@ -13,7 +13,7 @@ double Thermistor100k::get_temp(int pin)
    adc_value=analogRead(pin); //150901 Thermistor added
    for(int16_t i=1; i<table_size; i++) {
    if(table[i-1] <= adc_value && table[i] >= adc_value) {
-      temp_actual = (i*temp_step_size + start_temp);
+      temp_actual = ((i-1)*temp_step_size + start_temp);
 	  return  temp_actual;
 	}
    }
